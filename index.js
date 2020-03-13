2ac1a4b5dfc3bafce69e897309b9c4cc4b62faaf
Gabesz Rozsa <gabesz.rozsa@gmail.com>
2020-03-13 22:19:11 +0100

    saving job title and url

--- index.js
+++ index.js
@@ -3,10 +3,22 @@ const cheerio = require("cheerio");
 
 const url = "https://sfbay.craigslist.org/d/software-qa-dba-etc/search/sof";
 
+const scrapeResults = [];
+
 async function scrapeCraigslist() {
   try {
     const htmlResult = await request.get(url);
-    console.log(htmlResult);
+    const $ = cheerio.load(htmlResult);
+
+    $(".result-info").each((index, element) => {
+      const resultTitle = $(element).children(".result-title");
+
+      const title = resultTitle.text();
+      const url = resultTitle.attr("href");
+
+      const scrapeResult = { title, url };
+      scrapeResults.push(scrapeResult);
+    });
   } catch (error) {
     console.log(error);
   }
